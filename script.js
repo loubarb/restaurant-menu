442444e12533ddbaad00f069b1211f3dba917351
loubarb <louis.barbati@gmail.com>
2022-06-08 22:54:08 -0400

    Added logic to filter food items on click

---
+++ script.js
@@ -0,0 +1,23 @@
+const btns = document.querySelectorAll('.btn')
+const menuItems = document.querySelectorAll('.menu-item')
+
+for (i = 0; i < btns.length; i++) {
+
+  btns[i].addEventListener('click', e => {
+    e.preventDefault()
+    const filter = e.target.dataset.filter
+    // console.log(filter)
+
+    menuItems.forEach((item) => {
+      if (filter == "all") {
+        item.style.display = ""
+      } else {
+        if (item.classList.contains(filter)) {
+          item.style.display = ""
+        } else {
+          item.style.display = "none"
+        }
+      }
+    })
+  })
+}
